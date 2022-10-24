0aef2ef6bee134240a18912ad608c104206e6bdd
Zack Turman <zkturman@gmail.com>
2022-10-24 13:49:28 +0100

    fixed ui text size and added in-game brightness setting

--- Assets/UI/PauseMenu/PauseMenuUIController.cs
+++ Assets/UI/PauseMenu/PauseMenuUIController.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
 public class PauseMenuUIController : MonoBehaviour
 {
     private VisualElement rootVisualElement;
     private Button quitButton;
     private Button continueButton;
     private Button controlsButton;
+    private SliderInt brightnessSetting;
+    private int currentBrightness = 5;
+    private float minBrightness = -5;
+    private float maxBrightness = 5;
     private bool pressedContinue = false;
     [SerializeField]
     private GameObject controlsUI;
+    [SerializeField]
+    private Volume postProcessingVolume;
 
     private void OnEnable()
     {
@@ -21,6 +30,9 @@ public class PauseMenuUIController : MonoBehaviour
         controlsButton = rootVisualElement.Q<Button>("ControlsButton");
         controlsButton.RegisterCallback<ClickEvent>(ev => openControlsUI());
         controlsUI.SetActive(false);
+        brightnessSetting = rootVisualElement.Q<SliderInt>("BrightnessSetting");
+        brightnessSetting.RegisterValueChangedCallback(ev => setBrightness());
+        brightnessSetting.value = currentBrightness;
     }
 
     private void logPressedContinueAction()
@@ -31,6 +43,14 @@ public class PauseMenuUIController : MonoBehaviour
     private void openControlsUI()
     {
         controlsUI.SetActive(true);
+    }
+
+    private void setBrightness()
+    {
+        currentBrightness = brightnessSetting.value;
+        postProcessingVolume.profile.TryGet<ColorAdjustments>(out ColorAdjustments colorComponent);
+        //colorComponent.postExposure = new FloatParameter(currentBrightness - 5);
+        colorComponent.postExposure.Override(currentBrightness - 5);
     }
 
     public bool HasUserPressedContinue()
